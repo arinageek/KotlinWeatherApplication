d07a0e651b7faa6ce07782a6e288bf14b5eb5b78
Arina <arina435@mail.ru>
2021-10-03 16:12:56 +0300

    Designed fragment_home.xml

--- app/src/main/java/com/example/kotlinweatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinweatherapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.kotlinweatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinweatherapplication.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
