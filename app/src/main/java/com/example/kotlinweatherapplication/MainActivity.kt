cd01cba041f85889abb8a883a6ea4c2483fd648d
Arina <arina435@mail.ru>
2021-10-03 16:35:26 +0300

    Bottom navigation

--- app/src/main/java/com/example/kotlinweatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinweatherapplication/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.kotlinweatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.kotlinweatherapplication.ui.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val controller = fragment.navController
+        bottom_navigation_view.setupWithNavController(controller)
     }
 }
--- app/src/main/java/com/example/kotlinweatherapplication/ui/HomeFragment.kt
+++ app/src/main/java/com/example/kotlinweatherapplication/ui/HomeFragment.kt
@@ -1,7 +1,14 @@
 package com.example.kotlinweatherapplication.ui
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.kotlinweatherapplication.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
 }
